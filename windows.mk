0b1a12f854cf03d2966a7aab017677687c2d2b61
Austin Larsen <austin.larsen@mailfence.com>
2024-12-04 20:14:27 -0500

    opengl in windows

--- windows.mk
+++ windows.mk
@@ -17,7 +17,8 @@
 # 
 CC = x86_64-w64-mingw32-gcc
 
-LIBS = /mnt/c/Libraries/lua54/lua54.lib \
+LIBS = -lopengl32\
+	   /mnt/c/Libraries/lua54/lua54.lib \
 	   /mnt/c/Libraries/SDL2/SDL2.lib
 
 SHARED = -shared
